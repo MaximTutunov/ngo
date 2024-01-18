6a044caeb85ccda5991a2a83bb803d07464aae12
MaximTutunov <97736607+MaximTutunov@users.noreply.github.com>
2024-01-18 15:28:06 +0300

    Test page

---
+++ components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+    <h1>Nuxt JS</h1>
+    <p>Layout and Nuxt component</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        testData: ''
+      }
+    }
+  }
+</script>
+
+<style  scoped>
+main {
+  text-align: left;
+}
+</style>
